45c2130ba2235e1b02da41037b4f4ce5fcf32fa0
dsg <david@rek.com.mx>
2025-12-08 23:48:32 -0600

    Fix UI components (badge/button), update tailwind config, update sw.js and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,19 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   darkMode: "class",
-} satisfies Config;
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // aquí puedes ir metiendo customizaciones de colores, etc. si quieres
+    },
+  },
+  plugins: [],
+};
 
+export default config;
